8b7d60c29753d361060ddb09ca82e15bab648ab8
Alexander Schäfer <alexander.schaefer@uni-duesseldorf.de>
2016-11-20 23:33:33 +0100

    Create sqlite3 Database class with some tables

---
+++ hhcUserControl/database.py
@@ -0,0 +1,32 @@
+import sqlite3
+
+class Database(object):
+
+	def __init__(self):
+		self.connection = sqlite3.connect("hhcUserControl.db")
+		self.cursor = self.connection.cursor()
+		self.createTables()
+
+	def createTables(self):
+		self.cursor.execute(
+			"""CREATE TABLE IF NOT EXISTS `contact` (
+				`id` INTEGER PRIMARY KEY AUTOINCREMENT,
+				`prefix` TEXT,
+				`first_name` TEXT,
+				`last_name` TEXT,
+				`birth_date` DATE,
+				`comment` TEXT
+			)""")
+
+		self.cursor.execute(
+			"""CREATE TABLE IF NOT EXISTS `member` (
+				`contact_id` INTEGER PRIMARY KEY REFERENCES contact(id),
+				`ressort` TEXT,
+				`active` INTEGER(1),
+				`position` TEXT,
+				`joined` DATE,
+				`left` DATE
+			)""")
+
+if __name__ == "__main__":
+	db = Database()
