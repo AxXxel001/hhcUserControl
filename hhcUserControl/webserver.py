5c2a67a1a057f97c9c37133943c4e75e552c9b2e
Alexander Schäfer <alexander.schaefer@uni-duesseldorf.de>
2016-11-20 10:55:26 +0100

    Run first HelloWordServer

--- hhcUserControl/webserver.py
+++ hhcUserControl/webserver.py
@@ -1,1 +1,17 @@
-import tornado
+import tornado.web
+import tornado.ioloop
+
+class HelloWorldHandler(tornado.web.RequestHandler):
+	def get(self):
+		self.write("Hello, World!")
+
+app = tornado.web.Application([
+	(r"/", HelloWorldHandler),
+])
+app.listen(4455)
+print "Listening on localhost:8888..."
+try:
+	tornado.ioloop.IOLoop.current().start()
+except KeyboardInterrupt:
+	pass
+print "Done."
